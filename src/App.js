5bd1848314097ebcfaaef03aaaef25cb2f915984
TEJAS <tkundalia@live.co.uk>
2019-05-11 21:43:16 +0200

    submission

--- src/App.js
+++ src/App.js
@@ -1,70 +1,72 @@
 import React, {Component} from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import {createStore} from 'redux';
 import {connect} from 'react-redux';
 
-
 const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
+
 const data = {
-  "Ivel Z3": {
-    manufacturer: "Ivasim",
-    year: 1969,
-    origin: "Croatia"
-  },
-  "Bally Astrocade": {
-    manufacturer: "Bally Consumer Products",
-    year: 1977,
-    origin: "USA"
-  },
-  "Sord M200 Smart Home Computer": {
-    manufacturer: "Sord Computer Corporation",
-    year: 1971,
-    origin: "Japan"
-  },
-  "Commodore 64": {
-    manufacturer: "Commodore",
-    year: 1982,
-    origin: "USA"
+    "Ivel Z3": {
+      manufacturer: "Ivasim",
+      year: 1969,
+      origin: "Croatia"
+    },
+    "Bally Astrocade": {
+      manufacturer: "Bally Consumer Products",
+      year: 1977,
+      origin: "USA"
+    },
+    "Sord M200 Smart Home Computer": {
+      manufacturer: "Sord Computer Corporation",
+      year: 1971,
+      origin: "Japan"
+    },
+    "Commodore 64": {
+      manufacturer: "Commodore",
+      year: 1982,
+      origin: "USA"
+    }
   }
-}
-const reducer = (state = data, action) => {
-  switch (action.type) {
-    default:
-      return state
-  }
+  const reducer = (state = data, action) => {
+    switch (action.type) {
+      default:
+        return state
+    }
 }
 
 export const store = createStore(reducer, enhancer)
 
-class App extends Component {    
+class App extends React.Component {    
+  constructor (props) {
+    super(props);
+    this.state = {value: {}}
+    
+  }
+
   
-   updateSelection(event){
-    console.log('event:', event.target.value)
-    //console.log('Props:', this.props)                 //?????????
+    
+  updateSelection(event){
+    console.log('event:', event.target.value) 
+    //const selected= this.props.filter(selected => (selected.key === event.target.value))
   }
   
   render(){
-    //console.log('props in render', this.props.techlist)
     return (
-      <div>
-       {/* <h1>{Object.keys(data)}</h1>*/}
-      <form className="App-header" >     
-             
-              <br/>                 
-              <select onChange={this.updateSelection} value={this.props.value}>
-                
-                <option > -- Pick a model ---</option>   
-                {Object.keys(data).map (name =>
-                      <option value={name}> {name+ ' (' + data[name].year + ') '} </option>  
-                )}
-                  
-              </select>         
-              <button> Add </button> 
-           
-        </form>     
-      </div>
+    <div>
+      <form className="App-header" >                 
+        <br/>                 
+        <select onChange={this.updateSelection} value={this.props.value}>
+          
+          <option > -- Pick a model ---</option>   
+          {Object.keys(data).map (name =>
+                <option value={name}> {name+ ' (' + data[name].year + ') '} </option>  
+          )}
+
+        </select>         
+        <button> Add </button>            
+      </form>     
+     </div>
     );
     }
 }
